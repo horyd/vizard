a78a9fed98b59ebb54015cabb4c25a21a03775f0
William Archinal <william.archinal@foxsports.com.au>
2018-12-18 15:40:21 +1100

    MWL-485 | More sophisticated logic around page refreshes, improve wait for initial connection to page

--- src/util/take-screenshots.js
+++ src/util/take-screenshots.js
@@ -4,7 +4,10 @@ const getScreenshotPath = require('./screenshot-path');
 const logger = require('./logger');
 const runPromisesSequentially = require('./run-promises-sequentially');
 
-const PAGE_REFRESH_FREQUENCY = 10;
+// Refresh the pages at most every 20 tests to avoid puppeteer hanging
+const PAGE_REFRESH_FREQUENCY = 20;
+const SLOW_TEST_WARNING_TIME_MS = 7 * 1000 * PAGE_REFRESH_FREQUENCY;
+const NUM_TEST_RETRIES = 3;
 
 module.exports = async function takeScreenshots({
     config,
@@ -61,13 +64,41 @@ module.exports = async function takeScreenshots({
             // We reload the page every few tests (defined by PAGE_REFRESH_FREQUENCY), because sometimes Puppeteer hangs, and reloading mitigates that
             const chunkedTests = chunk(testsForWindow, PAGE_REFRESH_FREQUENCY);
 
-            await runPromisesSequentially(chunkedTests.map((tests) => async () => {
-                const runTestsOnWindow = (tests) => window._runTests({tests});
+            const resetPage = () => page.reload().then(() => page.evaluate(() => window._registerTests()));
 
-                await page.evaluate(runTestsOnWindow, tests);
+            const createTestSetRunner = (tests) => async () => {
+                // Try running the tests 3 times (restarting if they timeout)
+                const tryRunningTest = () => new Promise((resolve, reject) => {
+                    // If the test takes too long, reject the promise and try again
+                    const hungNodeProcessWarning = setTimeout(() => {
+                        logger.warn(`Thread ${threadNumber} at viewport ${viewportWidth}x${viewportHeight} has been running for more than ${Math.floor(SLOW_TEST_WARNING_TIME_MS / 1000)}s. Retrying.`);
+                        resetPage().finally(reject);
+                    }, SLOW_TEST_WARNING_TIME_MS);
 
-                await page.reload();
-            }));
+                    const runTestsOnWindow = (tests) => window._runTests({tests});
+
+                    page.evaluate(runTestsOnWindow, tests)
+                        .then(() => {
+                            clearTimeout(hungNodeProcessWarning);
+                            resolve();
+                        })
+                        .catch(reject);
+                });
+
+                let testRunningPromise = tryRunningTest();
+
+                for (let retryCount = 0; retryCount < NUM_TEST_RETRIES; retryCount++) {
+                    testRunningPromise = testRunningPromise.catch(tryRunningTest);
+                }
+
+                await testRunningPromise
+                    .catch(() => {
+                        logger.error(`Tried running screenshot test ${NUM_TEST_RETRIES} times with no success`, tests);
+                        throw new Error(`Screenshot set failed ${NUM_TEST_RETRIES} times`);
+                    });
+            };
+
+            await runPromisesSequentially(chunkedTests.map(createTestSetRunner));
         }));
     }));
 
